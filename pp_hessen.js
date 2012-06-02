bbf64d04c424613aa484bf3538dfc7efb4476a32
nowrap <nowrap@gmx.net>
2012-06-02 15:43:36 +0200

    fixed regional default text

--- pp_hessen.js
+++ pp_hessen.js
@@ -243,21 +243,36 @@ $(document).ready(function() {
 		pp_hessen_outHessen();
 		$("#pp_hessen_regionClose").hide();
 	});
-	//if (pp_hessen_default != 0)
-		pp_hessen_outHessen();
+
+	pp_hessen_outHessen();
 });
 
 function pp_hessen_outHessen() {
 	if (pp_hessen_clicked == "") {
-		$("#pp_hessen_karteHessen").css({backgroundPosition: "0 -"+229*pp_hessen_default+"px"});
-		$("#pp_hessen_regionTitle").html("Landesverband Hessen");
-		$("#pp_hessen_regionLine1").html("1.931 Piraten");
-		$("#pp_hessen_regionLine2").html("16 Kreisverbände");
-		$("#pp_hessen_regionLine3").html("32 Abgeordnete");
-		$("#pp_hessen_regionLine4").html("<a href='/mitmachen'>Mitmachen</a>");
+		if (pp_hessen_default == 0) {
+			$("#pp_hessen_karteHessen").css({backgroundPosition: "0px"});
+			$("#pp_hessen_regionTitle").html("Landesverband Hessen");
+			$("#pp_hessen_regionLine1").html("1.931 Piraten");
+			$("#pp_hessen_regionLine2").html("16 Kreisverbände");
+			$("#pp_hessen_regionLine3").html("32 Abgeordnete");
+			$("#pp_hessen_regionLine4").html("<a href='/mitmachen'>Mitmachen</a>");
+		} else {
+			var region = pp_hessen_getRegion();
+			pp_hessen_showTile(region);
+		}
 	} else {
 		pp_hessen_clickHessen(pp_hessen_clicked);
 	}
+}
+
+function pp_hessen_getRegion() {
+	var region = "";
+	$.each(pp_hessen, function(key, data) {
+		if (pp_hessen_default == data.offset)
+			region = key;
+	});
+	
+	return region;
 }
 
 function pp_hessen_overHessen(region) {
