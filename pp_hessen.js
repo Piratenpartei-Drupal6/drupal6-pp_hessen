77be8012fdd3c593a5d9e51e8a7a78677c1e9339
nowrap <nowrap@gmx.net>
2012-07-14 23:09:32 +0200

    lv fixes

--- pp_hessen.js
+++ pp_hessen.js
@@ -250,15 +250,14 @@ $(document).ready(function() {
 });
 
 function pp_hessen_outHessen() {
-	console.log(pp_hessen_default+" "+pp_hessen_clicked);
 	if (pp_hessen_clicked == "") {
 		if (pp_hessen_default == 0) {
-			$("#pp_hessen_karteHessen").css({backgroundPosition: "0px"});
 			$("#pp_hessen_regionTitle").html("Landesverband Hessen");
 			$("#pp_hessen_regionLine1").html("1.931 Piraten");
-			$("#pp_hessen_regionLine2").html("16 Kreisverbände");
-			$("#pp_hessen_regionLine3").html("32 Abgeordnete");
-			$("#pp_hessen_regionLine4").html("<a href='/mitmachen'>Mitmachen</a>");
+			$("#pp_hessen_regionLine2").html("<a href='http://www.piratenpartei-hessen.de/kreisverbaende'>16 Kreisverbände</a>");
+			$("#pp_hessen_regionLine3").html("<a href='http://www.piratenpartei-hessen.de/kommunalpolitik'>32 Abgeordnete</a>");
+			$("#pp_hessen_regionLine4").html("<a href='http://www.piratenpartei-hessen.de/mitmachen'>Mitmachen</a>");
+			$("#pp_hessen_karteHessen").css({backgroundPosition: "0 0"});
 		} else {
 			var region = pp_hessen_getRegion();
 			pp_hessen_showTile(region);
@@ -291,10 +290,10 @@ function pp_hessen_clickHessen(region) {
 }
 
 function pp_hessen_showTile(region) {
-	$("#pp_hessen_karteHessen").css({backgroundPosition: "0 -"+229*pp_hessen[region].offset+"px"});
 	$("#pp_hessen_regionTitle").html(pp_hessen[region].title ? pp_hessen[region].title : "&nbsp;");
 	$("#pp_hessen_regionLine1").html(pp_hessen[region].lines[0] ? pp_hessen[region].lines[0] : "&nbsp;");
 	$("#pp_hessen_regionLine2").html(pp_hessen[region].lines[1] ? pp_hessen[region].lines[1] : "&nbsp;");
 	$("#pp_hessen_regionLine3").html(pp_hessen[region].lines[2] ? pp_hessen[region].lines[2] : "&nbsp;");
 	$("#pp_hessen_regionLine4").html(pp_hessen[region].lines[3] ? pp_hessen[region].lines[3] : "&nbsp;");
+	$("#pp_hessen_karteHessen").css({backgroundPosition: "0 -"+229*pp_hessen[region].offset+"px"});
 }
